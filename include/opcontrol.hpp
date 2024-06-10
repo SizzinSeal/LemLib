4990ebf76e71f1bcfd55702167abfc0f67922d67
Liam Teale <liamteale6@gmail.com>
2024-06-10 09:49:17 -0700

    add settings constants for driver control

--- include/opcontrol.hpp
+++ include/opcontrol.hpp
@@ -2,8 +2,44 @@
 
 #include "chassis.hpp"
 
+/////////////////////////////////////////
+// EXTERN VARIABLES
+/////////////////////////
+
 // input curve for throttle input during driver control
 extern lemlib::ExpoDriveCurve throttleCurve;
 
 // input curve for steer input during driver control
 extern lemlib::ExpoDriveCurve steerCurve;
+
+///////////////////////////////////////
+// SETTINGS
+/////////////////////////
+
+constexpr int THROTTLE_UPPER_DEADBAND = 0; // upper joystick deadband out of 127
+constexpr int THROTTLE_LOWER_DEADBAND = 0; // lower joystick deadband out of 127
+constexpr int THROTTLE_MIN_OUTPUT = 12; // minimum output where drivetrain will move out of 127
+constexpr double THROTTLE_EXPO_GAIN = 1.019; // expo curve gain
+
+constexpr int STEER_UPPER_DEADBAND = 0; // upper joystick deadband out of 127
+constexpr int STEER_LOWER_DEADBAND = 0; // lower joystick deadband out of 127
+constexpr int STEER_MIN_OUTPUT = 15; // minimum output where drivetrain will move out of 127
+constexpr double STEER_EXPO_GAIN = 1.019; // expo curve gain
+
+constexpr double DESATURATE_BIAS = 0.75; // bias for desaturation. 0 prioritizes throttle, 1 prioritizes steering
+
+constexpr int INTAKE_IN_SPEED = 127; // speed for intaking
+constexpr int OUTTAKE_FAST_SPEED = 127; // speed for outtaking with wings
+constexpr int OUTTAKE_SLOW_SPEED = 50; // speed for outtaking without wings
+
+///////////////////////////////////////
+// CONTROLS
+/////////////////////////
+
+#define HANG_TOGGLE_BUTTON pros::E_CONTROLLER_DIGITAL_UP // button to toggle the hang mechanism
+#define LEFT_WING_BUTTON pros::E_CONTROLLER_DIGITAL_RIGHT // button to activate the left wing
+#define RIGHT_WING_BUTTON pros::E_CONTROLLER_DIGITAL_Y // button to activate the right wing
+#define INTAKE_IN_BUTTON pros::E_CONTROLLER_DIGITAL_L1 // button to intake
+#define INTAKE_OUT_BUTTON pros::E_CONTROLLER_DIGITAL_L2 // button to outtake
+#define THROTTLE_JOYSTICK pros::E_CONTROLLER_ANALOG_LEFT_Y // joystick for throttle
+#define STEER_JOYSTICK pros::E_CONTROLLER_ANALOG_RIGHT_X // joystick for steering
--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -2,18 +2,11 @@
 #include "devices.hpp"
 
 // input curve for throttle input during driver control
-lemlib::ExpoDriveCurve throttleCurve(0, // upper joystick deadband out of 127
-                                     0, // lower joystick deadband out of 127
-                                     12, // minimum output where drivetrain will move out of 127
-                                     1.019 // expo curve gain
-);
+lemlib::ExpoDriveCurve throttleCurve(THROTTLE_UPPER_DEADBAND, THROTTLE_LOWER_DEADBAND, THROTTLE_MIN_OUTPUT,
+                                     THROTTLE_EXPO_GAIN);
 
 // input curve for steer input during driver control
-lemlib::ExpoDriveCurve steerCurve(0, // upper joystick deadband out of 127
-                                  0, // lower joystick deadband out of 127
-                                  15, // minimum output where drivetrain will move out of 127
-                                  1.019 // expo curve gain
-);
+lemlib::ExpoDriveCurve steerCurve(STEER_UPPER_DEADBAND, STEER_LOWER_DEADBAND, STEER_MIN_OUTPUT, STEER_EXPO_GAIN);
 
 /**
  * Runs in driver control
@@ -25,25 +18,24 @@ void opcontrol() {
     // loop while in driver control
     while (true) {
         // hang mechanism
-        if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_UP)) hangToggle = !hangToggle;
+        if (controller.get_digital_new_press(HANG_TOGGLE_BUTTON)) hangToggle = !hangToggle;
         hang.set_value(hangToggle);
         // wings
-        if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_RIGHT)) leftWing.set_value(true);
+        if (controller.get_digital(LEFT_WING_BUTTON)) leftWing.set_value(true);
         else leftWing.set_value(false);
-        if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_Y)) rightWing.set_value(true);
+        if (controller.get_digital(RIGHT_WING_BUTTON)) rightWing.set_value(true);
         else rightWing.set_value(false);
         // intake
-        if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) intake.move(127);
-        else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {
-            if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_RIGHT) ||
-                controller.get_digital(pros::E_CONTROLLER_DIGITAL_Y))
-                intake.move(-127);
-            else intake.move(-50);
+        if (controller.get_digital(INTAKE_IN_BUTTON)) intake.move(127);
+        else if (controller.get_digital(INTAKE_OUT_BUTTON)) {
+            if (controller.get_digital(LEFT_WING_BUTTON) || controller.get_digital(RIGHT_WING_BUTTON))
+                intake.move(-OUTTAKE_FAST_SPEED);
+            else intake.move(-OUTTAKE_SLOW_SPEED);
         } else intake.move(0);
         // drivetrain
-        int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
-        int rightX = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
-        chassis.arcade(leftY, rightX, 0, 0.75);
+        int leftY = controller.get_analog(THROTTLE_JOYSTICK);
+        int rightX = controller.get_analog(STEER_JOYSTICK);
+        chassis.arcade(leftY, rightX, 0, DESATURATE_BIAS);
         // delay to save resources
         pros::delay(10);
     }
